c58ceef93e602f436d95b0f600787c5e06b9be32
ll0zlk <hyojin041025@gmail.com>
2024-11-20 18:22:30 +0900

    feat: UserDTO 추가 및 DTO 기반으로 변경

--- src/main/java/com/example/backendproject/service/UserService.java
+++ src/main/java/com/example/backendproject/service/UserService.java
@@ -1,8 +1,21 @@
 package com.example.backendproject.service;
 
+import com.example.backendproject.dto.UserDTO;
+import com.example.backendproject.entity.User;
+import com.example.backendproject.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO addUser(UserDTO userDTO) {
+        User user = new User();
+        User savedUser = userRepository.save(user);
+        return new UserDTO(savedUser.getUserName(), savedUser.getPhone());
+    }
 }
